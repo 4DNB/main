dde832fe15646f71028a03423843e0d8ff8cf4c9
4DNB <davidnb03@outlook.com>
2026-01-11 18:31:13 -0500

    state

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './Button.css'
 
 function Button({ texto }) {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
     return (
-        <div style={{ 
-            
+
+        <div style={{
+
             // border: '1px solid red',            
-         }}>
+        }}>
             <button style={{
             }}
                 className="BtnPrincipal"
+                onClick={handleClick}
             >
-                {texto}
+                {count}
             </button>
+            
         </div>
     )
 }
